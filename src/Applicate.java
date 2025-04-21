4fc6be19bea02efc7d7a33ccedaf393b1adb4b09
Mays Qh <maysqh@Mayss-MacBook-Pro.local>
2025-04-21 22:43:20 +0300

    Add 9+ new files to the project

---
+++ src/Applicate.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author maysqh
+ */
+public class Applicate {
+
+    private final Scanner scanner;
+    private final FileHandler fileHandler;
+
+    public Applicate(Scanner scanner, FileHandler fileHandler) {
+        this.scanner = scanner;
+        this.fileHandler = fileHandler;
+    }
+}
--- src/MunicipalityApp.java
+++ src/MunicipalityApp.java
@@ -1,7 +1,9 @@
+
 import java.io.*;
 import java.util.*;
 
 public class MunicipalityApp {
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         FileHandler fileHandler = new FileHandler("neighborhoods.txt");
@@ -9,9 +11,15 @@ public class MunicipalityApp {
         System.out.println("Welcome to Jeddah Municipality Complaint System");
         System.out.print("Are you an Applicant or Employee? (a/e): ");
         String role = scanner.nextLine();
-        
+
+        if (role.equalsIgnoreCase("a")) {
+            Applicate applicant = new Applicate(scanner, fileHandler);
+
+        } else if (role.equalsIgnoreCase("e")) {
+
+        } else {
+            System.out.println("Invalid role selected.");
+        }
+
     }
 }
-
-
-
