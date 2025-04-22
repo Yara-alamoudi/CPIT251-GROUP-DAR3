b06a6c0d5ddb8f5af1315d78815ca680b1c758dd
Zad A Alfayedi <160277956+ALfayediZAD@users.noreply.github.com>
2025-04-22 21:39:48 +0300

    Added 2 new methods

--- src/Applicate.java
+++ src/Applicate.java
@@ -18,4 +18,35 @@ public class Applicate {
         this.scanner = scanner;
         this.fileHandler = fileHandler;
     }
+    private String getUserId() {
+        System.out.print("Enter your National ID: ");
+        return scanner.nextLine();
+    }
+           private boolean Previous_Complaints(String userId) throws IOException {
+        File currentDir = new File(".");
+        File[] files = currentDir.listFiles();
+        boolean found = false;
+    
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                String fileName = file.getName();
+                if (fileName.startsWith("complaint_" + userId + "_") && fileName.endsWith(".txt")) {
+                    if (!found) {
+                        System.out.println("You have previous complaints:");
+                        found = true;
+                    }
+                    BufferedReader reader = new BufferedReader(new FileReader(file));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.startsWith("Status:")) {
+                            System.out.println(" - " + file.getName() + " | " + line);
+                            break;
+                        }
+                    }
+                    reader.close();
+                }
+            }
+        }
+        return found;
+    }
 }
